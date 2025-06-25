3d1a41d540993a4524a6f1930c0705afb65d6d1c
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2025-06-24 21:21:48 -0300

    Create IntroducaoDDL_DML.sql

---
+++ IntroducaoDDL_DML.sql
@@ -0,0 +1,26 @@
+-- Mostra todos os bancos dentro do SGBD
+show databases;
+
+-- Cria um banco de dados
+create database aula;
+
+-- Após criar o banco, precisamos indicar que queremos usá-lo
+use aula;
+
+-- Como confirmar o banco selecionado
+select database();
+
+-- Criando uma tabela
+create table exemplo(
+    id int,
+    nome varchar(100)
+);
+
+-- Mostrando tabelas
+show tables;
+
+-- Inserindo dados na tabela
+insert into exemplo(id, nome) values(1, 'Ana');
+
+-- Realizando uma consulta na tabela
+select * from exemplo;
